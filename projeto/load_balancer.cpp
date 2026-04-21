7dc6fe5f4862dda153c35b60ae2ba7be58928f77
ViniciusMassuda <viniciusmassuda@estudante.ufscar.br>
2026-04-21 16:00:56 -0300

    feat: implementa round robin

--- projeto/load_balancer.cpp
+++ projeto/load_balancer.cpp
@@ -14,26 +14,41 @@ int main() {
     bind(server_fd, (struct sockaddr*)&address, sizeof(address));
     listen(server_fd, 3);
 
-    std::cout << "LB rodando\n";
+    std::cout << "Load Balancer rodando na porta 8080\n";
+
+    // 🔹 backends disponíveis
+    int ports[] = {9001, 9002};
+    int current = 0;
 
     while (true) {
         int client_socket = accept(server_fd, nullptr, nullptr);
+
+        // 🔹 escolhe backend (round robin)
+        int backend_port = ports[current];
+        current = (current + 1) % 2;
+
+        std::cout << "Encaminhando para porta " << backend_port << std::endl;
 
         int backend_socket = socket(AF_INET, SOCK_STREAM, 0);
 
         sockaddr_in backend_addr;
         backend_addr.sin_family = AF_INET;
-        backend_addr.sin_port = htons(9001);
+        backend_addr.sin_port = htons(backend_port);
         backend_addr.sin_addr.s_addr = INADDR_ANY;
 
         connect(backend_socket, (struct sockaddr*)&backend_addr, sizeof(backend_addr));
 
+        // 🔹 lê requisição do cliente
         char buffer[30000];
         int size = read(client_socket, buffer, 30000);
 
+        // 🔹 envia para backend
         send(backend_socket, buffer, size, 0);
 
+        // 🔹 recebe resposta do backend
         size = read(backend_socket, buffer, 30000);
+
+        // 🔹 envia de volta para cliente
         send(client_socket, buffer, size, 0);
 
         close(client_socket);
